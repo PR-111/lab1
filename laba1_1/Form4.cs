5e7ebfc810942788cbf983aa1e9419938acb904c
jak0796 <jak0796@gmail.com>
2015-03-26 09:23:39 +0200

    Revert "Код майже готовий"
    This reverts commit 3f4385dcfdf4c040553bdbc09b124842a776f5d8.

---
+++ laba1_1/Form4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba1_1
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void postBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void postBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.postBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.dataSet1);
+
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dataSet1.post' table. You can move, or remove it, as needed.
+            this.postTableAdapter.Fill(this.dataSet1.post);
+
+        }
+    }
+}
